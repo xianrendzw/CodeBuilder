7fe13f7f9369eed86f81f966fe77e4718820342f
TomDeng <xianrendzw@hotmail.com>
2011-11-30 18:42:23 +0800

    Create projects

---
+++ CodeBuilder.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeBuilder.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
