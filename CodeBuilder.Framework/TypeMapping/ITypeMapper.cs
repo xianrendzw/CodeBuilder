4b899702a4c5b61079460d09a8895530fafd0bcc
TomDeng <xianrendzw@hotmail.com>
2012-01-29 08:48:21 +0800

    fixed Templates ItemGroup

--- CodeBuilder.Framework/TypeMapping/ITypeMapper.cs
+++ CodeBuilder.Framework/TypeMapping/ITypeMapper.cs
@@ -9,13 +9,6 @@ namespace CodeBuilder.TypeMapping
 
     public interface ITypeMapper
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param typeName="database"></param>
-        /// <param typeName="value"></param>
-        /// <param typeName="dbDataTypeName"></param>
-        /// <returns></returns>
         LanguageType GetLanguageType(string database, string language, string dbDataTypeName);
     }
 }
