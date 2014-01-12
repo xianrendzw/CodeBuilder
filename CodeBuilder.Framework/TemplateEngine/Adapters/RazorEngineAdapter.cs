803f62e5659eaa0c4a5b5f06538869d09e6500df
xianrendzw <14068728@qq.com>
2014-01-12 11:39:28 +0800

    update

--- CodeBuilder.Framework/TemplateEngine/Adapters/RazorEngineAdapter.cs
+++ CodeBuilder.Framework/TemplateEngine/Adapters/RazorEngineAdapter.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using RazorEngine;
-using RazorEngine.Templating;
+using System.Web.Razor.Utils;
 
 namespace CodeBuilder.TemplateEngine
 {
